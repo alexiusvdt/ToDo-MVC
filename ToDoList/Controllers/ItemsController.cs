22e3360758bded293c181e747a0426465eff7981
Alex Johnson <alex.johnson293@gmail.com>
2022-12-14 10:35:25 -0800

    refactor items as children of Categories

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -6,39 +6,53 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-
-    [HttpGet("/items")]
-    public ActionResult Index()
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
-    public ActionResult New()
+      Category category = Category.Find(categoryId);
+      return View(category);
+    }    
+  
+  
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      return View();
-    }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
-
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
   }
+
+    // removed as Items all belong to Category objects now
+    // impossible to just create an item
+    // [HttpGet("/items")]
+    // public ActionResult Index()
+    // {
+    //   List<Item> allItems = Item.GetAll();
+    //   return View(allItems);
+    // }
+    // [HttpPost("/items/delete")]
+    // public ActionResult DeleteAll()
+    // {
+    //   Item.ClearAll();
+    //   return View();
+    // }
+    // [HttpGet("/items/new")]
+    // public ActionResult New()
+    // {
+    //   return View();
+    // }    
+
+  //this is the old, original Show() method before 
+  // we housed Items in Categories
+  //  [HttpGet("/items/{id}")]
+  //   public ActionResult Show(int id)
+  //   {
+  //     Item foundItem = Item.Find(id);
+  //     return View(foundItem);
+  //   }    
+
 }
