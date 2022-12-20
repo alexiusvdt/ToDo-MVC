d935f1dec346d80b6603881816e700a0dac1939c
Alex Johnson <alex.johnson293@gmail.com>
2022-12-19 17:07:38 -0800

    WIP eod progress

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int Id { get; }
+    public int Id { get; set; }
 
     public Item (string description)
     {
@@ -28,8 +29,9 @@ namespace ToDoList.Models
       else
       {
         Item newItem = (Item) otherItem;
+        bool idEquality = (this.Id == newItem.Id);
         bool descriptionEquality = (this.Description == newItem.Description);
-        return descriptionEquality;
+        return (idEquality && descriptionEquality);
       }
     }
     
@@ -38,12 +40,72 @@ namespace ToDoList.Models
         return Id.GetHashCode();
     }
 
-    public static Item Find(int searchId)
+    public static Item Find(int id)
     {
-      // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
-      Item placeholderItem = new Item("placeholder item");
-      return placeholderItem;
+      // We open a connection.
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      conn.Open();
+
+      // We create MySqlCommand object and add a query to its CommandText property. 
+      // We always need to do this to make a SQL query.
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = "SELECT * FROM items WHERE id = @ThisId;";
+
+      // We have to use parameter placeholders @ThisId and a `MySqlParameter` object to 
+      // prevent SQL injection attacks. 
+      // This is only necessary when we are passing parameters into a query. 
+      // We also did this with our Save() method.
+      MySqlParameter param = new MySqlParameter();
+      param.ParameterName = "@ThisId";
+      param.Value = id;
+      cmd.Parameters.Add(param);
+
+      // We use the ExecuteReader() method because our query will be returning results and 
+      // we need this method to read these results. 
+      // This is in contrast to the ExecuteNonQuery() method, which 
+      // we use for SQL commands that don't return results like our Save() method.
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemDescription = "";
+      while (rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemDescription = rdr.GetString(1);
+      }
+      Item foundItem = new Item(itemDescription, itemId);
+
+      // We close the connection.
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return foundItem;
     }
+
+    public void Save()
+    {
+      MySqlConnection conn = new MySqlConnection(DBConfiguration.ConnectionString);
+      conn.Open();
+
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      // shorter way would be like this
+      // cmd.Parameters.AddWithValue("@ItemDescription", this.Description);
+      cmd.CommandText = "INSERT INTO items (description) VALUES (@ItemDescription);";
+      MySqlParameter param = new MySqlParameter();
+      param.ParameterName = "@ItemDescription";
+      param.Value = this.Description;
+      cmd.Parameters.Add(param);    
+      cmd.ExecuteNonQuery();
+      Id = (int) cmd.LastInsertedId;
+
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
 
     public static void ClearAll()
     {
